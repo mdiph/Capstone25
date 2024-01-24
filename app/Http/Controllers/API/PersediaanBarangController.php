a09d2e7114dbc520a8e5d7e24d382c03ee698913
MnoorIbra <ibralincah69@gmail.com>
2024-01-24 21:27:21 +0700

    update API 1.1

--- app/Http/Controllers/API/PersediaanBarangController.php
+++ app/Http/Controllers/API/PersediaanBarangController.php
@@ -19,18 +19,36 @@ class PersediaanBarangController extends Controller
     public function index()
     {
 
-        $data = DB::select('SELECT
-        p.nama_produk,
-        COALESCE(sr.stok,0) AS stok_awal,
-       COALESCE(bm.jumlah_masuk,0) AS stok_masuk,
-        COALESCE(bk.jumlah_keluar,0) AS stok_keluar,
-        COALESCE((sr.stok + COALESCE(bm.jumlah_masuk,0) - COALESCE(bk.jumlah_keluar,0) ),0) AS stok_akhir
-      FROM produk p
-      LEFT JOIN barang_masuk bm ON p.id = bm.produk_id
-      LEFT JOIN barang_keluar bk ON p.id = bk.produk_id
-      LEFT JOIN produk_record sr ON p.id = sr.produk_id
-      GROUP BY p.id;');
+    //     $data = DB::select('SELECT
+    //     p.nama_produk,
+    //     COALESCE(sr.stok,0) AS stok_awal,
+    //    COALESCE(bm.jumlah_masuk,0) AS stok_masuk,
+    //     COALESCE(bk.jumlah_keluar,0) AS stok_keluar,
+    //     COALESCE((sr.stok + COALESCE(bm.jumlah_masuk,0) - COALESCE(bk.jumlah_keluar,0) ),0) AS stok_akhir
+    //   FROM produk p
+    //   LEFT JOIN barang_masuk bm ON p.id = bm.produk_id
+    //   LEFT JOIN barang_keluar bk ON p.id = bk.produk_id
+    //   LEFT JOIN produk_record sr ON p.id = sr.produk_id
+    //   GROUP BY p.id;');
 
+    $date = Carbon::today();
+
+    $query = "SELECT
+    p.id,
+    p.nama_produk,
+    COALESCE(sr.stok, p.stok) AS stok_awal,
+    COALESCE(bm.jumlah_masuk, 0) AS stok_masuk,
+    COALESCE(bk.stok_keluar, 0) AS stok_keluar,
+    COALESCE((sr.stok + COALESCE(bm.jumlah_masuk, 0) - COALESCE(bk.stok_keluar, 0)), 0) AS stok_akhir
+FROM produk p
+LEFT JOIN produk_record sr ON p.id = sr.produk_id AND sr.tanggal = ?
+LEFT JOIN barang_masuk bm ON p.id = bm.produk_id AND bm.tanggal_masuk = ?
+LEFT JOIN transaksi bk ON p.id = bk.produk_id AND bk.tanggal_transaksi = ?
+GROUP BY p.id, p.nama_produk, sr.stok, bm.jumlah_masuk, bk.stok_keluar
+";
+
+
+    $data = DB::select($query, [$date, $date, $date]);
 
 
 
@@ -47,7 +65,7 @@ if ($data) {
     public function query($date)
     {
 
-        $tanggal = Carbon::today();
+        // $date = Carbon::today();
 
         $query = "SELECT
         p.id,
